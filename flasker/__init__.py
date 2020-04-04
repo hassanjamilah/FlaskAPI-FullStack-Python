a99d6bc05f6624b152dc7d07e0d713674c04a8a5
hassan.jamila <hassan.jamila@n2nconnect.com>
2020-04-04 15:27:00 +0800

    Handle the root and send simple json response

--- flasker/__init__.py
+++ flasker/__init__.py
@@ -2,4 +2,9 @@ from flask import Flask , jsonify
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    
+    @app.route('/')
+    def hello():
+        return jsonify({'message':'Hello World!'})
+    
     return app 
